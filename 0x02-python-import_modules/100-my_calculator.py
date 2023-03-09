6a09c8311e3c8e71b54da0cfb833609f93ac15df
hedishafi <hafsahussein1221@gmail.com>
2023-03-09 08:27:11 +0300

    task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
+    """A calculator to handle arguments and perfom basic operations"""
     import sys
+    from calculator_1 import add, sub, mul, div
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print value of imported variable"""
+
     from variable_load_5 import a
 
-    print(a)
+    print(f'{a}')
